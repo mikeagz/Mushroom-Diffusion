83d93f3204c0c3d63a8ca2fa6f2c84224fe0d4b2
mikeagz <150627929+mikeagz@users.noreply.github.com>
2023-11-29 19:07:15 -0600

    Updated src

---
+++ src/convert_files2ds.py
@@ -0,0 +1,5 @@
+# De ser necesario el dataset almacenado en disco se transforma a 
+# dataset de huggingFace para usarlo en otros experimentos
+
+# Se espera que este script lea los archivos, cree una instancia un 
+# hf dataset y lo suba a un repositorio
